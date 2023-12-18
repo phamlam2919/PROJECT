8ffe127565ce660c7ec8333bf148ed1cba410031
phamlam2919 <lp76500@gmail.com>
2023-12-18 08:58:42 +0700

    update

--- CLient/src/App.jsx
+++ CLient/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./Pages/register/Register";
+import Register1 from "./Pages/register/Register1";
+import Register2 from "./Pages/register/Register2";
 
 function App() {
     return (
         <>
-            <Register />
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/register1" element={<Register1 />} />
+                <Route path="/register2" element={<Register2 />} />
+            </Routes>
         </>
     );
 }
